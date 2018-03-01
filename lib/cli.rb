0868961dc2dda2f379a5ea32ce42c1b072945181
Austin Bourgerie <austin@bourg.me>
2018-02-28 22:03:03 -0500

    Removed vestiges of old configuration system. Tidied up cli.rb.

--- lib/cli.rb
+++ lib/cli.rb
@@ -8,37 +8,42 @@ require 'common/logger'
 
 require 'config/global_config'
 require 'config/assignment_config'
-require 'config/ingest_config'
 
 module SPD
   class CLI < Thor
-    include SPD::Ops
+    include SPD
 
-    $global_config_location = 'global_config'
-    $assignment_config_location = 'assignment_config'
+    $global_config_location = File.expand_path('~/.spd_global_config.toml')
+    $assignment_config_location = 'assignment_config.toml'
 
     desc 'sanity', 'Create a testing setup that checks for expected files'
     option :files, {:required => true, :aliases => '-f', :type => :array}
     option :output, {:aliases => '-o', :type => :string, :default => 'sanity'}
 
     def sanity
-      Sanity.do_sanity(options[:output], options[:files])
+      Ops::Sanity.do_sanity(options[:output], options[:files])
     end
 
     desc 'distribute', 'Distribute submissions to graders'
 
     def distribute
-      config = SPD::Config::GlobalConfig.load_from_file($global_config_location)
-      SPD::Ops::Distribute.do_distribute(config)
+      Ops::Distribute.do_distribute(load_global_config)
     end
 
     desc 'ingest', 'Interactively parse graded files to create uploadable CSV + redistributables'
 
     def ingest
-      global_config = SPD::Config::GlobalConfig.load_from_file($global_config_location)
-      local_config = SPD::Config::AssignmentConfig.load_from_file($assignment_config_location)
+      Ops::Ingester.new(load_global_config, load_assignment_config).do_ingest
+    end
 
-      SPD::Ops::Ingester.new(global_config, local_config).do_ingest
+    private
+
+    def load_global_config
+      Config::GlobalConfig.load_from_file($global_config_location)
+    end
+
+    def load_assignment_config
+      Config::AssignmentConfig.load_from_file($assignment_config_location)
     end
   end
 end
--- lib/config/loadable_by_eval.rb
+++
@@ -1,9 +0,0 @@
-module SPD
-  module Config
-    module LoadableByEval
-      def load_from_disk(path)
-        SPD::Common::FileOps.eval_file(path, self)
-      end
-    end
-  end
-end
